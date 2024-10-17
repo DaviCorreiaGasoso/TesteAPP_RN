67b340e7c5a73ed318e50b98205474143afd20eb
DaviCorreiaGasoso <davicorreiao07@gmail.com>
2024-10-17 17:43:06 -0300

    Desenvolvimento do app

---
+++ PrimeiroApp/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, TextInput, TouchableOpacity, SytleSheet, Image} from "react-native";
+import { SafeAreaView } from "react-native-web";
+import estiloso from CSS;
+
+const TelaEntrar = () => {
+  return (
+    <SafeAreaView style={estiloso.background}>
+      <View style={estiloso.contair}>
+
+      <Image
+      source="../Imagens/icone_login.png"
+      style={estiloso.icone_login}
+      />
+      
+      <View style={estiloso.inputView}>
+        <TextInput
+          style={estiloso.texto_input}
+          placeholder= "Login"
+          placeholderTextColor="#003f5c"
+          />
+      </View>
+      <View style={estiloso.inputView}>
+        <TextInput
+          style={estiloso.texto_input}
+          placeholder= "Senha"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+        />
+      </View>
+      <TouchableOpacity style={estiloso.botao}></TouchableOpacity>
+
+
+
+    </SafeAreaView>
+  )
+}
